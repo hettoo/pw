68c2655aa294f2cd019ee23b70d6c60bb8b64743
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-07-15 00:33:29 +0200

    Split off page functions
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- index.php
+++ index.php
@@ -33,39 +33,6 @@ function import_lib($lib) {
     import_once("lib/$lib");
 }
 
-function page_file($page) {
-    return 'page' . $page;
-}
-
-function real_page($page) {
-    global $args;
-    while ($page != $args[0] && is_null(script(page_file($page)))) {
-        $next_page = dirname($page);
-        $page = $next_page . '/_';
-        if (is_null(script(page_file($page))))
-            $page = $next_page;
-    }
-    if ($page == $args[0])
-        $page .= '404';
-    return $page;
-}
-
-function init_child_page($child) {
-    global $args;
-    init_page(real_page($args) . '/' . $child);
-}
-
-function import_child_page($child, $header = true) {
-    global $args, $s, $hierarchy;
-    $page = real_page($args) . '/' . $child;
-    $hierarchy[1] = $child;
-    if ($header) {
-        init_page($page);
-        echo '<h2>' . $s['head'] . '</h2>';
-    }
-    import_page($page);
-}
-
 function resource_url($target) {
     global $base;
     return $base . (substr($base, -1) == '/' ? '' : '/') . 'r/' . $target;
@@ -96,16 +63,6 @@ function url($target, $level = 0, $rootify = true) {
 function this_url() {
     global $hierarchy;
     return url(join('/', $hierarchy));
-}
-
-function init_page($page) {
-    $file = page_file(real_page($page) . '_init');
-    if (file_exists(script($file)))
-        import_once($file);
-}
-
-function import_page($page) {
-    import(page_file(real_page($page)));
 }
 
 function read_line($fp) {
--- lib/content.php
+++ lib/content.php
@@ -2,6 +2,49 @@
 
 import_lib('format');
 
+function page_file($page) {
+    return 'page' . $page;
+}
+
+function real_page($page) {
+    global $args;
+    while ($page != $args[0] && is_null(script(page_file($page)))) {
+        $next_page = dirname($page);
+        $page = $next_page . '/_';
+        if (is_null(script(page_file($page))))
+            $page = $next_page;
+    }
+    if ($page == $args[0])
+        $page .= '404';
+    return $page;
+}
+
+function init_page($page) {
+    $file = page_file(real_page($page) . '_init');
+    if (file_exists(script($file)))
+        import_once($file);
+}
+
+function import_page($page) {
+    import(page_file(real_page($page)));
+}
+
+function init_child_page($child) {
+    global $args;
+    init_page(real_page($args) . '/' . $child);
+}
+
+function import_child_page($child, $header = true) {
+    global $args, $s, $hierarchy;
+    $page = real_page($args) . '/' . $child;
+    $hierarchy[1] = $child;
+    if ($header) {
+        init_page($page);
+        echo '<h2>' . $s['head'] . '</h2>';
+    }
+    import_page($page);
+}
+
 header('Content-Type: text/html; charset=iso-8859-1');
 
 $s['submenu'] = '';
