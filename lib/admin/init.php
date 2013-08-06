a0931673f527da8987fceda15f12d8a2042611f2
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-08-06 20:03:09 +0200

    Fixed overview add links
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- lib/admin/init.php
+++ lib/admin/init.php
@@ -7,15 +7,15 @@ $s['admin_level'] = 5;
 
 add_module('Config', $s['admin_level'], array(
     array('list', 'List config'),
-    array('add', 'Add config value')
+    array('edit', 'Add config value')
 ));
 add_module('Users', $s['cooperator_level'], array(
     array('list', 'List users'),
-    array('add', 'Add user')
+    array('edit', 'Add user')
 ));
 add_module('Pages', $s['editor_level'], array(
     array('list', 'List pages'),
-    array('add', 'Add page')
+    array('edit', 'Add page')
 ));
 
 ?>
