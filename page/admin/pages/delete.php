1a904aafeb1c4833a2b3e9ca9a3a186f1b1d8778
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-08-06 19:50:54 +0200

    Prevent orphan content
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- page/admin/pages/delete.php
+++ page/admin/pages/delete.php
@@ -9,6 +9,7 @@ $s['suburl'] = array('id');
 $id = find_value('id');
 if (isset($id)) {
     $id = (int)$id;
+    query("DELETE FROM `content` WHERE `page`=$id");
     query("DELETE FROM `page` WHERE `id`=$id");
 }
 redirect_up();
