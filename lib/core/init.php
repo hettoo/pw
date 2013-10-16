1b6547b5253625c554106192e649c3caf2d4fa21
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-10-16 17:50:42 +0200

    Moved actual config initialisation
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- lib/core/init.php
+++ lib/core/init.php
@@ -9,20 +9,7 @@ $hierarchy = explode('/', $args);
 
 $s = array();
 
-$s['project'] = 'Personal Website';
-$s['analytics'] = '';
-
-$s['charset'] = 'iso-8859-1';
-$s['menu'] = ':Home,Admin';
-$s['theme'] = 'default';
-$s['css'] = 'main';
-$s['max_pages'] = 8;
-
-$s['header'] = '';
-$s['suburl'] = array();
-
-$s['prefix'] = 'pw_';
-
+import_lib('core/init_config');
 import_lib('core/database');
 
 if (file_exists('setup/setup')) {
---
+++ lib/core/init_config.php
@@ -0,0 +1,17 @@
+<?php
+
+$s['project'] = 'Personal Website';
+$s['analytics'] = '';
+
+$s['charset'] = 'iso-8859-1';
+$s['menu'] = ':Home,Admin';
+$s['theme'] = 'default';
+$s['css'] = 'main';
+$s['max_pages'] = 8;
+
+$s['header'] = '';
+$s['suburl'] = array();
+
+$s['prefix'] = 'pw_';
+
+?>
