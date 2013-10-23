ee20eaec22e006325e9b4b95136924625dcdb52c
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-10-23 23:53:57 +0200

    Static menus
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- section/main.php
+++ section/main.php
@@ -5,23 +5,14 @@
 </head>
 <body>
         <div id="main">
-            <div id="header">
-            </div>
-            <div id="first">
-            </div>
-            <div id="main-menu">
-                <?= create_menu(0, $s['menu']); ?>
-            </div>
-            <div class="clear"></div>
-            <div id="head">
-                <h1><?= $s['head']; ?></h1>
-            </div>
-            <div id="sub-menu">
-                <?= $s['submenu']; ?>
-            </div>
+            <div id="header"></div>
+            <div id="first"></div>
             <div id="content">
+                <h1><?= $s['head'] ?></h1>
                 <?php body(); ?>
             </div>
+            <div id="main-menu"><?= create_menu(0, $s['menu']); ?></div>
+            <div id="sub-menu"><?= $s['submenu']; ?></div>
         </div>
 </body>
 </html>
