9545f10b1b6d9430e7c8cca9400ef60df81a3b74
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-10-24 18:16:35 +0200

    Moved content up
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- section/main.php
+++ section/main.php
@@ -5,14 +5,14 @@
 </head>
 <body>
         <div id="main">
-            <div id="header"></div>
-            <div id="first"></div>
             <div id="content">
                 <h1><?= $s['head'] ?></h1>
                 <?php body(); ?>
             </div>
             <div id="main-menu"><?= create_menu(0, $s['menu']); ?></div>
             <div id="sub-menu"><?= $s['submenu']; ?></div>
+            <div id="header"></div>
+            <div id="first"></div>
         </div>
 </body>
 </html>
