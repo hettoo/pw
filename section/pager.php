d4d552cbb9bc1a691ae135d5223177bd6047c56b
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-11-03 13:50:34 +0100

    Pager layout fix
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- section/pager.php
+++ section/pager.php
@@ -9,18 +9,18 @@ list($start, $end) = $pager->getRange();
 $pages = $pager->getPages();
 
 ?>
-<ul class="pager">
-    <li><a href="<?= url(1, $index, false) ?>">&lt;&lt;</a></li>
-    <li><a href="<?= url(max($page - 1, 1), $index, false) ?>">&lt;</a></li>
+<div class="pager">
+    <span><a href="<?= url(1, $index, false) ?>">&lt;&lt;</a></span>
+    <span><a href="<?= url(max($page - 1, 1), $index, false) ?>">&lt;</a></span>
     <?php if ($start > 1): ?>
         ...
     <?php endif ?>
     <?php for ($i = $start; $i <= $end; $i++): ?>
-        <li<?= ($i == $page ? ' class="active"' : '') ?>><a href="<?= url($i, $index, false) ?>"><?= $i ?></a></li>
+        <span<?= ($i == $page ? ' class="active"' : '') ?>><a href="<?= url($i, $index, false) ?>"><?= $i ?></a></span>
     <?php endfor ?>
     <?php if ($end < $pages): ?>
         ...
     <?php endif ?>
-    <li><a href="<?= url(min($page + 1, $pages), $index, false) ?>">&gt;</a></li>
-    <li><a href="<?= url($pages, $index, false) ?>">&gt;&gt;</a></li>
-</ul>
+    <span><a href="<?= url(min($page + 1, $pages), $index, false) ?>">&gt;</a></span>
+    <span><a href="<?= url($pages, $index, false) ?>">&gt;&gt;</a></span>
+</div>
