b33c945799b50839e96976329c540b72c8d91d0d
Gerco van Heerdt <gercovanheerdt@gmail.com>
2013-11-18 12:53:58 +0100

    Hide pager stats without results
    Signed-off-by: Gerco van Heerdt <gercovanheerdt@gmail.com>

--- section/pager_stats.php
+++ section/pager_stats.php
@@ -3,6 +3,8 @@
 list($pager, $type) = $s['d'];
 
 ?>
+<?php if ($pager->getTotal()): ?>
 <p>
 Displaying <?= $pager->getDisplayed() ?> / <?= $pager->getTotal() ?> <?= $type ?>.
 </p>
+<?php endif ?>
